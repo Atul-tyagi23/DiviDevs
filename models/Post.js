8c642227acbfba003c93f65940f134df80a705d0
Atul-tyagi23 <63414418+Atul-tyagi23@users.noreply.github.com>
2020-12-19 11:52:51 +0530

    Added Post model

---
+++ models/Post.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const { schema } = require('./Profile');
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    avatar: {
+        type: String
+    },
+    likes :[
+        {
+            user: Schema.Types.ObjectId,
+            ref: 'user'
+        }
+    ],
+    comments: [
+        {
+          user: {
+            type: Schema.Types.ObjectId
+          },
+          text: {
+            type: String,
+            required: true
+          },
+          name: {
+            type: String
+          },
+          avatar: {
+            type: String
+          },
+          date: {
+            type: Date,
+            default: Date.now
+          }
+        }
+      ],
+      date: {
+        type: Date,
+        default: Date.now
+      }
+});
+
+module.exports = Post =  mongoose.model('post', PostSchema);
