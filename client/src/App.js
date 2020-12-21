afb1863ea1fc367085ee17b7e205d2149a339023
Atul-tyagi23 <63414418+Atul-tyagi23@users.noreply.github.com>
2020-12-21 18:46:19 +0530

    react added

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+ import './App.css';
+ import React, {Fragment} from 'react'
+const App = () => 
+    <div className="App">
+      <Fragment>
+      <h1>App</h1>
+      </Fragment>
+      
+    </div>
+ 
+
+export default App;
